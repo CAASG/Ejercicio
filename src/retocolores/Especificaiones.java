c42ebbc39dac209dcda34e2eb706316259572447
Aula L 5-2 <l52@academico.unab.ad>
2022-10-20 11:09:00 -0500

    Ejemplo de como subir el proyecto

---
+++ src/retocolores/Especificaiones.java
@@ -0,0 +1,9 @@
+package retocolores;
+
+public class Especificaiones {
+    private int cantidadColores;
+    private String nombrePlanta;
+    private int numeroFlores;
+    private int cantidadEspinas;
+    
+}
